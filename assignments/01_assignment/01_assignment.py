2e65b2e24ea79bb5c550d8df90bbfd3e122f0d18
suriija <64418683+suriija@users.noreply.github.com>
2020-04-29 11:55:02 +0200

    .
    .

--- assignments/01_assignment/01_assignment.py
+++ assignments/01_assignment/01_assignment.py
@@ -28,10 +28,48 @@ def rgb_to_hsv(x):
     else:
         s = df / mx
     v = mx
-
     array.shape[0] = h
     array.shape[1] = s
     array.shape[2] = v
+    x = array
+    return x
+
+
+
+def hsv_to_rgb(x):
+    img = x
+    array = np.array(img)
+    h = array.shape[0]
+    s = array.shape[1]
+    v = array.shape[2]
+
+    h = float(h)
+    s = float(s)
+    v = float(v)
+    h60 = h / 60.0
+    h60f = math.floor(h60)
+    hi = int(h60f) % 6
+    f = h60 - h60f
+    p = v * (1 - s)
+    q = v * (1 - f * s)
+    t = v * (1 - (1 - f) * s)
+    r, g, b = 0, 0, 0
+    if hi == 0:
+        r, g, b = v, t, p
+    elif hi == 1:
+        r, g, b = q, v, p
+    elif hi == 2:
+        r, g, b = p, v, t
+    elif hi == 3:
+        r, g, b = p, q, v
+    elif hi == 4:
+        r, g, b = t, p, v
+    elif hi == 5:
+        r, g, b = v, p, q
+    r, g, b = int(r * 255), int(g * 255), int(b * 255)
+    array.shape[0] = r
+    array.shape[1] = g
+    array.shape[2] = b
     x = array
     return x
 
